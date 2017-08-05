53e85eff45c7a166a2c8032859879284e31c1baa
amparish <alexmparish@gmail.com>
2017-08-05 13:15:40 -0500

    all functions working, no styling

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,39 +2,36 @@ var db = require("../models");
 var express = require('express');
 var app = express.Router();
 
-	app.get("/", function(req, res){
-		db.burger.findAll({}).then(function(data){
-			var hbsObject = {burger: data};
-			console.log(hbsObject);
-			res.render("index", data);
-		});
+app.get("/", function(req, res){
+	db.burger.findAll({}).then(function(data){
+		res.render("index", {burgers: data});
 	});
+});
 
-	app.post("/", function(req, res){
-		console.log(req.body);
-		db.burger.create({
-			burger_name: req.body.name,
-			devoured: false
-		}).then(function(data){
-			console.log(data)
-			res.redirect("/");
-		});
+app.post("/", function(req, res){
+	console.log(req.body);
+	db.burger.create({
+		burger_name: req.body.name,
+		devoured: false
+	}).then(function(data){
+		console.log(data);
+		res.redirect("/");
 	});
+});
 
-	app.put("/:id", function(req, res){
-		console.log(req.body.id);
+app.put("/:id", function(req, res){
+	console.log(req.params.id);
 
-		db.burger.update({
-			devoured: true
-		}, {
-			where: {
-				id: req.body.id
-			}
-		}).then(function(data){
-			console.log(data);
-			res.redirect("/");
-		});
+	db.burger.update({
+		devoured: true
+	}, {
+		where: {
+			id: req.params.id
+		}
+	}).then(function(data){
+		console.log(data);
+		res.redirect("/");
 	});
+});
 
-	module.exports = app;
-	
+module.exports = app;
